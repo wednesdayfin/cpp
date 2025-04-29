177744f6216d0f134d3fe227f0bc748d0be668ea
wednesdayfin <raimovormisto86@gmail.com>
2025-04-29 05:27:42 +0300

    Update lab2_1.cpp

--- lab2_1.cpp
+++ lab2_1.cpp
@@ -14,18 +14,3 @@ int multiply(int a, double b, bool roundFlag) {
     double result = a * b;
     return roundFlag ? static_cast<int>(round(result)) : static_cast<int>(result);
 }
-
-int main() {
-
-    cout << "multiply(3, 4) = " << multiply(3, 4) << endl;
-
-    cout << "multiply(2.5, 4.2) = " << multiply(2.5, 4.2) << endl;
-
-    cout << "multiply(3, 2.7, false) = " << multiply(3, 2.7, false) << endl;
-    cout << "multiply(3, 2.7, true) = " << multiply(3, 2.7, true) << endl;
-
-    cout << "multiply(3, 2.3, false) = " << multiply(3, 2.3, false) << endl;
-    cout << "multiply(3, 2.3, true) = " << multiply(3, 2.3, true) << endl;
-
-    return 0;
-}
