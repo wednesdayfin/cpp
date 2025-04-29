d510c0a5e52710199195f1c90d197b897f410aed
wednesdayfin <raimovormisto86@gmail.com>
2025-04-29 04:59:50 +0300

    Update lab2_1.cpp

--- lab2_1.cpp
+++ lab2_1.cpp
@@ -19,15 +19,13 @@ int main() {
 
     cout << "multiply(3, 4) = " << multiply(3, 4) << endl;
 
-    // B. Double multiplication
     cout << "multiply(2.5, 4.2) = " << multiply(2.5, 4.2) << endl;
 
-    // C. Integer and double multiplication with round flag
-    cout << "multiply(3, 2.7, false) = " << multiply(3, 2.7, false) << endl; // Output: 8
-    cout << "multiply(3, 2.7, true) = " << multiply(3, 2.7, true) << endl;  // Output: 8
+    cout << "multiply(3, 2.7, false) = " << multiply(3, 2.7, false) << endl;
+    cout << "multiply(3, 2.7, true) = " << multiply(3, 2.7, true) << endl;
 
-    cout << "multiply(3, 2.3, false) = " << multiply(3, 2.3, false) << endl; // Output: 6
-    cout << "multiply(3, 2.3, true) = " << multiply(3, 2.3, true) << endl;  // Output: 7
+    cout << "multiply(3, 2.3, false) = " << multiply(3, 2.3, false) << endl;
+    cout << "multiply(3, 2.3, true) = " << multiply(3, 2.3, true) << endl;
 
     return 0;
 }
