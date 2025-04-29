4b8454f1c24b3e604079eb36eb78857016b1752e
wednesdayfin <raimovormisto86@gmail.com>
2025-04-29 05:55:25 +0300

    Update lab2_6.cpp

--- lab2_6.cpp
+++ lab2_6.cpp
@@ -16,7 +16,6 @@ int main() {
     int choice;
 
     do {
-        cout << "\n--- Student Management Menu ---\n";
         cout << "1. Add a student\n";
         cout << "2. Display all students\n";
         cout << "3. Search student by ID\n";
@@ -32,7 +31,7 @@ int main() {
                 }
                 Student s;
                 cout << "Enter name: ";
-                cin.ignore(); // Clear newline from input buffer
+                cin.ignore();
                 cin.getline(s.name, 50);
 
                 cout << "Enter ID: ";
@@ -41,14 +40,12 @@ int main() {
                 cin >> s.grade;
 
                 students[studentCount++] = s;
-                cout << "Student added successfully!\n";
                 break;
             }
             case 2: {
                 if (studentCount == 0) {
                     cout << "No students to display.\n";
                 } else {
-                    cout << "\n--- Student List ---\n";
                     for (int i = 0; i < studentCount; ++i) {
                         cout << "Name: " << students[i].name
                              << ", ID: " << students[i].id
@@ -65,7 +62,6 @@ int main() {
 
                 for (int i = 0; i < studentCount; ++i) {
                     if (students[i].id == searchId) {
-                        cout << "Student found:\n";
                         cout << "Name: " << students[i].name
                              << ", Grade: " << students[i].grade << endl;
                         found = true;
@@ -82,7 +78,7 @@ int main() {
                 cout << "Exiting.\n";
                 break;
             default:
-                cout << "Invalid choice. Try again.\n";
+                cout << "Invalid choice.\n";
         }
 
     } while (choice != 4);
