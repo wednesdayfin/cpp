da3e83aae5d3ddc41659a00109fe0f824cb74e63
wednesdayfin <raimovormisto86@gmail.com>
2025-04-30 13:30:31 +0300

    Update lab3_2.cpp

--- lab3_2.cpp
+++ lab3_2.cpp
@@ -10,33 +10,32 @@ private:
     string group = "2025 group";
 
 public:
-
     void setName(string n) {
         name = n;
     }
-
+    
     void setAge(int a) {
         age = a;
     }
-
+    
     void setGrade(char g) {
         grade = g;
     }
-
+    
     string getName() {
         return name;
     }
-
+    
     int getAge() {
         return age;
     }
-
+    
     char getGrade() {
         return grade;
     }
-
+    
     void displayInfo() {
-        cout << "Student Info:" << endl;
+        cout << "Student Information:" << endl;
         cout << "Name: " << getName() << endl;
         cout << "Age: " << getAge() << endl;
         cout << "Grade: " << getGrade() << endl;
@@ -48,14 +47,14 @@ int main() {
     Student student1;
 
     student1.setName("John Doe");
-    student1.setAge(20);
+    student1.setAge(18);
     student1.setGrade('A');
 
     cout << "Name: " << student1.getName() << endl;
     cout << "Age: " << student1.getAge() << endl;
     cout << "Grade: " << student1.getGrade() << endl;
-
+    
     student1.displayInfo();
-
+    
     return 0;
 }
