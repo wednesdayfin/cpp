436aa18dba2c079e08b7fec8f48b1fb16749aadd
wednesdayfin <raimovormisto86@gmail.com>
2025-04-30 13:44:55 +0300

    Create lab4_4.cpp

---
+++ lab4_4.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+class Complex {
+private:
+    double real;
+    double imag;
+
+public:
+    // Constructor
+    Complex(double r = 0.0, double i = 0.0) : real(r), imag(i) {}
+
+    // Overload + operator
+    Complex operator+(const Complex& other) const {
+        return Complex(real + other.real, imag + other.imag);
+    }
+
+    // Overload - operator
+    Complex operator-(const Complex& other) const {
+        return Complex(real - other.real, imag - other.imag);
+    }
+
+    // Overload / operator
+    Complex operator/(const Complex& other) const {
+        double denominator = other.real * other.real + other.imag * other.imag;
+        return Complex(
+            (real * other.real + imag * other.imag) / denominator,
+            (imag * other.real - real * other.imag) / denominator
+        );
+    }
+
+    // Friend function to overload << operator
+    friend ostream& operator<<(ostream& os, const Complex& c);
+};
+
+// Definition of friend function
+ostream& operator<<(ostream& os, const Complex& c) {
+    os << c.real;
+    if (c.imag >= 0) {
+        os << " + " << c.imag << "i";
+    } else {
+        os << " - " << -c.imag << "i";
+    }
+    return os;
+}
+
+int main() {
+    Complex c1(4, 5), c2(2, -3);
+    cout << "c1 = " << c1 << endl;
+    cout << "c2 = " << c2 << endl;
+    cout << "Sum = " << (c1 + c2) << endl;
+    cout << "Difference = " << (c1 - c2) << endl;
+    cout << "Quotient = " << (c1 / c2) << endl;
+    return 0;
+}
